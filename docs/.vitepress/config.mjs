abbdc10ebfc95dc4e6fbcd78502b87d4a80134ce
CXPhoenix <jate9229@gmail.com>
2025-07-15 16:30:39 +0800

    ✨feat: 完善專案基本設定 📦
    - 📝 新增 package.json 描述、作者等資訊
    - 🚀 將專案類型更改為 module 以支援 ES Module
    - 🏷️ 更新 Vitepress 網站標題與描述

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  title: "A Newbie-friendly Vitepress Template Project",
+  description: "It's a vitepress template project for newbie 👶.",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
